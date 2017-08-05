f92ea10d748aa8c28c40ffbf2c8f8f52e0cdbe4f
Ryan Templin <tempjuice@hotmail.com>
2017-08-05 10:29:19 -0500

    jQuery added. Created input fields, buttons, listener. Tested all 3.

--- public/client.js
+++ public/client.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-
+    $('.mathButtons').on('click', function () {
+        console.log('A math button has been clicked');
+        
+        var inputOne = $('#inputOne').val();
+        var inputTwo = $('#inputTwo').val();
+        console.log(inputOne);
+        console.log(inputTwo);
+    });
+    
 });

967c8fde708c5c8f61712946a86d733f449a1d09
Ryan Templin <tempjuice@hotmail.com>
2017-08-05 11:02:16 -0500

    Object created on client side. Installed body-parser. Object successfully sent to server with POST.

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ var port = 5000;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.post('/math', function (req,res) {
+	console.log('Math post hit');
+	console.log(req.body);
+	
+})
 
 app.listen(port, function() {
 	console.log('listening on port', port);
--- public/client.js
+++ public/client.js
@@ -1,11 +1,21 @@
 $(document).ready(function () {
     $('.mathButtons').on('click', function () {
         console.log('A math button has been clicked');
-        
         var inputOne = $('#inputOne').val();
         var inputTwo = $('#inputTwo').val();
-        console.log(inputOne);
-        console.log(inputTwo);
+        var mathType = $(this).attr('id');
+        var domMathObject = {
+            input1: inputOne,
+            input2: inputTwo,
+            type: mathType
+        };
+
+        $.ajax({
+            method: 'POST',
+            url: '/math',
+            data: domMathObject,
+        })
+        
     });
     
 });
